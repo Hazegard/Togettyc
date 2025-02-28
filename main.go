731e5383921172eefd74e2dfdd8a68da961bf57e
Hazegard <m.catrice@outlook.com>
2025-02-28 23:19:25 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"bytes"
+	"embed"
+	"fmt"
+	"github.com/buildkite/terminal-to-html/v3"
+	"io"
+	"maze.io/x/ttyrec"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/alecthomas/kong"
+)
+
+type Config struct {
+	Date       bool   `help:"Show date" short:"d" default:"false"`
+	Html       bool   `help:"Display result in HTML" short:"H" default:"false"`
+	RecordFile string `help:"Dashboard page" default:"all" arg:""`
+}
+
+//go:embed record.html.tmpl
+var html_template embed.FS
+
+type TemplateData struct {
+	Config Config
+	Frames []Frame
+}
+
+func main() {
+	print(uint8((7 & 0xff_00) >> 8))
+	config := Config{}
+	_ = kong.Parse(&config)
+
+	var r io.ReadCloser
+	if config.RecordFile == "" {
+		r = os.Stdin
+	} else {
+		var err error
+		if r, err = os.Open(config.RecordFile); err != nil {
+			fatalf("error opening %s: %v\n", config.RecordFile, err)
+		}
+	}
+	defer r.Close()
+
+	d := ttyrec.NewDecoder(r)
+
+	m := Record{
+		Decoder:      d,
+		Frames:       []frame{},
+		CurrentFrame: 0,
+	}
+
+	records := m.ReadAll(config)
+	if config.Html {
+		tmpl, err := html_template.ReadFile("record.html.tmpl")
+		if err != nil {
+			fatalf("error loading HTML template: %v\n", err)
+		}
+		t := template.Must(template.New("HTML Record").Funcs(template.FuncMap{"FormatDate": DateFormater(config), "Ansi2Html": terminal.Render}).Parse(string(tmpl)))
+		if err != nil {
+			fatalf("error loading HTML template: %v\n", err)
+		}
+
+		templateData := TemplateData{
+			Config: config,
+			Frames: records,
+		}
+		err = t.Execute(os.Stdout, templateData)
+		if err != nil {
+			fatalf("error rendering HTML template: %v\n", err)
+		}
+	} else {
+		var result [][]byte
+		for _, frame := range records {
+			if config.Date {
+				content := append([]byte(FormatDate(frame.Date)+": "), frame.Data...)
+				result = append(result, content)
+			} else {
+				result = append(result, frame.Data)
+			}
+		}
+		_, err := io.WriteString(os.Stdout, string(bytes.Join(result, []byte("\n"))))
+		if err != nil {
+			fatalf("writing to stdout: %v\n", err)
+		}
+	}
+}
+
+func DateFormater(config Config) func(time.Time) string {
+	if !config.Date {
+		return func(time.Time) string {
+			return ""
+		}
+	}
+	return func(date time.Time) string {
+		return FormatDate(date)
+	}
+}
+
+func FormatDate(date time.Time) string {
+	return date.Format("2006-01-02 15:04:05")
+}
+
+func fatalf(format string, v ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, v...)
+	os.Exit(1)
+}
---
+++ ttyrec.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bytes"
+	"maze.io/x/ttyrec"
+	"time"
+)
+
+type frame struct {
+	Data []byte
+	Date time.Time
+}
+
+type Frame struct {
+	Data []byte
+	Date time.Time
+}
+
+func NewFrameFromTtyrec(f *ttyrec.Frame) frame {
+	return frame{
+		Data: f.Data,
+		Date: time.Unix(int64(f.Time.Seconds), int64(f.Time.MicroSeconds)*1000),
+	}
+}
+
+type Record struct {
+	Decoder      *ttyrec.Decoder
+	Frames       []frame
+	CurrentFrame int
+}
+
+func (m *Record) ReadAll(cfg Config) []Frame {
+	frames, stop := m.Decoder.DecodeStream()
+	defer stop()
+	var timestamps []time.Time
+	var datas [][]byte
+
+	currentData := []byte{}
+	currentTs := time.Time{}
+
+	for f := range frames {
+
+		bb := bytes.Split(f.Data, []byte("\n"))
+
+		i0 := 0
+		// Handle if the previous frame does not end with a newline
+		// To concatenate the peace of frames in on timestamp
+		if currentData != nil {
+			// If the last frame has remaining element
+			// append the first line of the current frame to the last frame element
+			currentData = append(currentData, bb[0]...)
+			// If we have more than one line in the current frame
+			// THe first line of the current frame is not the last
+			// So we can append the concatenation to the full data
+			// As well as the current timestamp (which is the timestamp of the previous frame
+			// then we set the initialization value to 1 to skep the first frame element
+			// Finally, we reset the buffers
+			if len(bb) > 1 {
+				datas = append(datas, currentData)
+				timestamps = append(timestamps, currentTs)
+				i0 = 1
+				currentData = []byte{}
+				currentTs = time.Time{}
+			} else {
+				// If there is only one element, we replace the first element of the frame with the
+				// concatenated one, and we proceed with the remaining elements
+				bb[0] = currentData
+			}
+		}
+
+		for i := i0; i < len(bb)-1; i++ {
+			// Skip empty elements
+			if len(bb[i]) == 0 {
+				continue
+			}
+			datas = append(datas, bb[i])
+			timestamps = append(timestamps, time.Unix(int64(f.Time.Seconds), int64(f.Time.MicroSeconds)*1000))
+		}
+		currentData = bb[len(bb)-1]
+		currentTs = time.Unix(int64(f.Time.Seconds), int64(f.Time.MicroSeconds)*1000)
+	}
+
+	// var res []string
+
+	newFrames := []Frame{}
+	for i := 0; i < len(datas); i++ {
+		data := bytes.TrimRight(datas[i], " ")
+
+		data = bytes.ReplaceAll(data, []byte("\033[2J\033[3J\033[H"), []byte(""))
+
+		f := Frame{
+			Data: data,
+			Date: timestamps[i],
+		}
+
+		newFrames = append(newFrames, f)
+
+		/*if cfg.Date {
+			f.Data = fmt.Sprintf("%20s: %s\n",  string(data))
+		} else {
+			f.Data = fmt.Sprintf("%s\n", string(data))
+		}*/
+	}
+	return newFrames
+}
